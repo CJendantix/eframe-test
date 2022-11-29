ae81d67032dffc95c89c8ba47407b95a5b504ce1
Connor Gilroy <cgilroy33@gmail.com>
2022-11-29 16:56:10 -0500

    Cleaned up comments

--- src/app.rs
+++ src/app.rs
@@ -1,22 +1,28 @@
+
+/// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
+
+// if we add new fields, give them default values when deserializing old state
 #[serde(default)]
 
+
 pub struct Calculator {
-    //Peristant Variables
+    // Create Variables
 }
 
 
+// Turn struct: Calculator into template: Default
 impl Default for Calculator {
     fn default() -> Self {
         Self {
-            //Instanciate Persitant variables
+            //Instanciate Variables
         }
     }
 }
 
 
 impl Calculator {
-    //Persistance
+    /// Persistance, Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         if let Some(storage) = cc.storage {
             return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
@@ -28,10 +34,14 @@ impl Calculator {
 
 
 impl eframe::App for Calculator {
-    /// Save State Before Shutdown
+
+
+    /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
+
+
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let Self {} = self;
 
